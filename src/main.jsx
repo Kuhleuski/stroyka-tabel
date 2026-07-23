2838e64c660f9bc8f7668cf12f19fca095c70bf0
Kuhleuski <84250276+Kuhleuski@users.noreply.github.com>
2026-07-24 01:05:50 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // Импортируем глобальные стили через модуль
-import './styles/globals.module.css'
+import './styles/variables.module.css'
+import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
