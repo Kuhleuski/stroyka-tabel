f4e5edf351cdb50a60a03df362b397eec749cb9b
Kuhleuski <84250276+Kuhleuski@users.noreply.github.com>
2026-07-25 16:40:33 +0300

    Update NotificationBadge.jsx

--- src/components/Layout/NotificationBadge.jsx
+++ src/components/Layout/NotificationBadge.jsx
@@ -9,7 +9,11 @@ export function NotificationBadge({ unreadCount, onClick }) {
                 <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/>
                 <path d="M13.73 21a2 2 0 0 1-3.46 0"/>
             </svg>
-            {hasUnread && <span className={styles.notificationBadgeDot}></span>}
+            {hasUnread && (
+                <span className={styles.notificationBadgeDot}>
+                    {unreadCount > 9 ? '9+' : unreadCount}
+                </span>
+            )}
         </button>
     )
 }
